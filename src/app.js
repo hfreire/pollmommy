f009c14f2728c7b7b4a2870d957ecda5d9e27974
Hugo Freire <hfreire@exec.sh>
2017-02-04 11:02:06 +0100

    Fixed eslint's shebang issue. Added eslint to make sure we have shebang.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+#!/usr/bin/env node
+
 /*
  * Copyright (c) 2017, Hugo Freire <hugo@exec.sh>.
  *
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-#!/usr/bin/env node
 
 process.on('uncaughtException', error => {
   console.error(error)
